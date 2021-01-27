a88b63b6f24361e70de372353746c1633f4a260d
Renato Oliveira <renato.oliveira83@outlook.com>
2021-01-27 10:06:38 -0300

    Project created

---
+++ finaltest/src/main/java/com/devsuperior/finaltest/FinaltestApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.finaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaltestApplication.class, args);
+	}
+
+}
